bcb973dab56bd7a60586449512015387f32712cd
Narjisha <narjishakuniyil@gmail.com>
2024-08-23 14:26:27 +0530

    admins

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var studentsRouter = require('./routes/studentsRoutes');
 var courseRouter = require('./routes/courseRoute')
+var adminRouter = require('./routes/admin')
 
 var app = express();
 connectDB()
@@ -30,6 +31,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/students', studentsRouter);
 app.use('/course',courseRouter)
+app.use('/admin',adminRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
