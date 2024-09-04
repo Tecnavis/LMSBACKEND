cfb42e1e5d268243937752369cd7e29282ca45b9
Ashiquetkorambayil <102928710+Ashiquetkorambayil@users.noreply.github.com>
2024-09-05 04:18:36 +0530

    edited the function of update students

--- Model/studentsModel.js
+++ Model/studentsModel.js
@@ -19,7 +19,7 @@ const studentSchema = new mongoose.Schema({
     maritalStatus: { type: String  },
     academicQualification: { type: String  },
     mobileNumber: { type: Number  },
-    parentsMobileNumber: { type: Number  },
+    parentsMobileNumber: { type: String ,default: null,},
     email: { type: String  },
     courseName: {  type: String},
     joinDate: { type: Date  },
